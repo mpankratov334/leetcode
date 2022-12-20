3d0e97fa4304969bc3b354b874a1d8be0b8dde92
mpankratov334 <101637853+mpankratov334@users.noreply.github.com>
2022-12-21 00:00:42 +0300

    Create 136_SingleNumberEasy.py

---
+++ 136_SingleNumberEasy.py
@@ -0,0 +1,16 @@
+"""
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+"""
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        """ time: O(n*log n) extraspace: O(1) """
+        length = len(nums)
+        if length == 1: return nums[0]
+        nums.sort()
+        for i in range(1, length, 2):
+            if nums[i] != nums[i - 1]: 
+                return nums[i-1]
+        return nums[-1]
