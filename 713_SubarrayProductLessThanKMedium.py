144f1f7d1be549c2aec6bc99d64bd709d43df678
mpankratov334 <101637853+mpankratov334@users.noreply.github.com>
2022-12-27 19:24:51 +0300

    Update 713_SubarrayProductLessThanKMedium.py

--- 713_SubarrayProductLessThanKMedium.py
+++ 713_SubarrayProductLessThanKMedium.py
@@ -1,11 +1,10 @@
 """
 Given an array of integers nums and an integer k, return the number of contiguous subarrays where
 the product of all the elements in the subarray is strictly less than k.
-
-
 """
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        """ O(n) """
         now_product = 1
         left = 0
         cnt = 0
