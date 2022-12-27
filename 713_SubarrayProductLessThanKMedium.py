407a3a24cf57140017e2be93d73e33d21d08f8b9
mpankratov334 <101637853+mpankratov334@users.noreply.github.com>
2022-12-27 19:20:33 +0300

    Create 713_SubarrayProductLessThanKMedium.py

---
+++ 713_SubarrayProductLessThanKMedium.py
@@ -0,0 +1,21 @@
+"""
+Given an array of integers nums and an integer k, return the number of contiguous subarrays where
+the product of all the elements in the subarray is strictly less than k.
+
+
+"""
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        now_product = 1
+        left = 0
+        cnt = 0
+        for right, val in enumerate(nums):
+            now_product *= val
+            while now_product >= k and left < right:
+                now_product /= nums[left]
+                left += 1
+
+            if now_product < k:
+                cnt +=right - left + 1
+        return cnt
+            
